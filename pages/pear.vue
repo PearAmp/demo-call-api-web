cd32be9d7a1e91e589ba78d75453a5908b7ab924
Nu Chaimongkon <nu.chaimongkon@devmaewmaew.io>
2024-03-12 17:08:16 +0700

    test

--- pages/pear.vue
+++ pages/pear.vue
@@ -1,12 +1,36 @@
 <template>
-  <div></div>
+  <div class="header">
+    <h2>a = {{total}}</h2>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'PearPage',
+  data () {
+    return {
+      first: 1,
+      second: 2,
+    }
+  },
+   computed: {
+    total () {
+      return this.first * this.second
+    }
+    
+  },
+  mounted () {
+  },
+ 
+    methods: {
+    
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
+.header {
+  margin-top: 4rem;
+}
 </style>
